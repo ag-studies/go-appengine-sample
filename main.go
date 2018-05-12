d5ac6cbea90a6ea893c60f31184d35f3cef2dac2
Ari Garcia <aristofanio@hotmail.com>
2018-05-12 18:53:16 -0300

    Initial Import

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/taskqueue"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	t := taskqueue.NewPOSTTask("/worker", map[string][]string{"key": {"val"}})
+	_, err := taskqueue.Add(ctx, t, "")
+	if err != nil {
+		log.Errorf(ctx, "Failed to add task")
+	}
+	fmt.Fprintf(w, "Success")
+}
+
+func Worker(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	log.Infof(ctx, "Worker succeeded")
+}
+
+func init() {
+	http.HandleFunc("/worker", Worker)
+	http.HandleFunc("/", Handler)
+}
